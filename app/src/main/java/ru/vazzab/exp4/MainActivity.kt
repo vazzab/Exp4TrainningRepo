1f4f4472e0016180406af6a95ca0c11d18292b94
vazzab <vazzyaaa@gmail.com>
2025-03-29 12:44:53 +0900

    добавил 3 активити, там будут проверяться задания с урока 20

--- app/src/main/java/ru/vazzab/exp4/MainActivity.kt
+++ app/src/main/java/ru/vazzab/exp4/MainActivity.kt
@@ -2,13 +2,37 @@ package ru.vazzab.exp4
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity() : AppCompatActivity(), Parcelable {
+    constructor(parcel: Parcel) : this() {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<MainActivity> {
+        override fun createFromParcel(parcel: Parcel): MainActivity {
+            return MainActivity(parcel)
+        }
+
+        override fun newArray(size: Int): Array<MainActivity?> {
+            return arrayOfNulls(size)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -24,8 +48,13 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        val button2 = findViewById<Button>(R.id.button2)
+        button2.setOnClickListener {
+            // Создаём Intent для перехода
+            val intent = Intent(this, View20_1Activity::class.java)
+            startActivity(intent) // Запускаем новое активити
 
 
-
+        }
     }
 }
---
+++ app/src/main/java/ru/vazzab/exp4/View20_1Activity.kt
@@ -0,0 +1,11 @@
+package ru.vazzab.exp4
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class View20_1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.view_20_1) // Привязываем макет
+    }
+}
